4d7a24018c15bb64f29326586b9bdba517dfe188
Michael Jarrett <jarremic@umich.edu>
2018-06-18 16:36:47 -0400

    Adds plotting to the read_power.py routine.

--- read_power.py
+++ read_power.py
@@ -2,11 +2,21 @@ import h5py
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+import sys
+from subprocess import call
 
-def print_attrs(name, obj):
-    print name
-    for key, val in obj.attrs.iteritems():
-        print "    %s: %s" % (key, val)
+def make_hist(datain, n):
+    dataout = np.zeros([2*n]) 
+    if(datain.size == n):
+        for i in range(0,n):
+            dataout[2*i]   = datain[i]
+            dataout[2*i+1] = datain[i]
+    elif(datain.size == n+1):
+        for i in range(0,n):
+            dataout[2*i]   = datain[i]
+            dataout[2*i+1] = datain[i+1]
+    return dataout
+    
 
 if __name__ == '__main__':
 
@@ -116,15 +126,18 @@ if __name__ == '__main__':
     pin_error = np.zeros([nmeth+2,nx,nz])
     k_error = np.zeros([nmeth+2])
     for im in range(0,nmeth): 
+        #print "method = %s " % methods[im]
         tmpsum = 0.0
         maxerror = 0.0
         for ix in range(0,nx):
             for iz in range(0,nz):
                 err = mpact_pinpow[im,ix,iz]-ref_pinpow[ix,iz]
-                pin_error[im,ix,iz] = err
+                pin_error[im,iz,ix] = err
+                #print " %8.4f" % err,
                 tmpsum = tmpsum + err*err
                 if(abs(err) > maxerror):
                     maxerror = abs(err)
+            #print " \n",
 
         rms_error[im] = math.sqrt(tmpsum / ncells)
         max_error[im] = maxerror
@@ -136,7 +149,7 @@ if __name__ == '__main__':
     for ix in range(0,nx):
         for iz in range(0,nz):
             err = sp3_pinpow[ix,iz]-ref_pinpow[ix,iz]
-            pin_error[im,ix,iz] = err
+            pin_error[im,iz,ix] = err
             tmpsum = tmpsum + err*err
             if(abs(err) > maxerror):
                 maxerror = abs(err)
@@ -151,7 +164,7 @@ if __name__ == '__main__':
     for ix in range(0,nx):
         for iz in range(0,nz):
             err = diffusion_pinpow[ix,iz]-ref_pinpow[ix,iz]
-            pin_error[im,ix,iz] = err
+            pin_error[im,iz,ix] = err
             tmpsum = tmpsum + err*err
             if(abs(err) > maxerror):
                 maxerror = abs(err)
@@ -198,8 +211,364 @@ if __name__ == '__main__':
     # plot errors #
     ###############     
 
-    #Xmesh = np.linspace(0,15.0,num=nx+1)
-    #Ymesh = np.linspace(0,15.0,num=nz+1)
-    #plt.pcolor(Xmesh,Ymesh,pin_error[0,:,:],colormap=ooolwarm)
+    clim_lower_bound = [-0.015,-0.005,-0.005,-0.001,-0.01,-0.10]
+    clim_upper_bound = [0.015,0.005,0.005,0.001,0.01,0.10]
+
+    Xmesh = np.linspace(0.0,15.0,num=nx+1)
+    Ymesh = np.linspace(0.0,15.0,num=nz+1)
+
+    fig = plt.figure(0)
+    plt.rc('font', size = 16)
+    plt.pcolor(Xmesh,Ymesh,pin_error[0,:,:],cmap='coolwarm')
+    plt.xticks(Xmesh)
+    plt.yticks(Ymesh)
+    plt.axis([0,15.0,0,15.0])
+    titlestr = "Power Shape Error, %s" % methods[0]
+    plt.title(titlestr)
+    plt.xlabel('Axial Height [cm]')
+    plt.ylabel('Power Shape Error (%)')
+    plt.clim([clim_lower_bound[0],clim_upper_bound[0]])
+    plt.colorbar()
+    #plt.show()
+
+    filename = "takeda_30cm_power_error_%s.png" % methods[0]
+    #filename = "takeda_30cm_power_error_%s.eps" % methods[0]
+    dirname = "figures/%s/" % methods[0]
+    call(["mkdir","-p",dirname])
+    savename = "%s/%s" % (dirname,filename)
+    #fig.savefig(savename, format='eps', dpi=1000)
+    fig.savefig(savename, format='png')
+    plt.close(fig)
+
+    fig = plt.figure(1)
+    plt.rc('font', size = 16)
+    plt.pcolor(Xmesh,Ymesh,pin_error[1,:,:],cmap='coolwarm')
+    plt.xticks(Xmesh)
+    plt.yticks(Ymesh)
+    plt.axis([0,15.0,0,15.0])
+    titlestr = "Power Shape Error, %s" % methods[1]
+    plt.title(titlestr)
+    plt.xlabel('Axial Height [cm]')
+    plt.ylabel('Power Shape Error (%)')
+    plt.clim([clim_lower_bound[1],clim_upper_bound[1]])
+    plt.colorbar()
+    #plt.show()
+
+    filename = "takeda_30cm_power_error_%s.png" % methods[1]
+    #filename = "takeda_30cm_power_error_%s.eps" % methods[1]
+    dirname = "figures/%s/" % methods[1]
+    call(["mkdir","-p",dirname])
+    savename = "%s/%s" % (dirname,filename)
+    #fig.savefig(savename, format='eps', dpi=1000)
+    fig.savefig(savename, format='png')
+    plt.close(fig)
+
+    fig = plt.figure(2)
+    plt.rc('font', size = 16)
+    plt.pcolor(Xmesh,Ymesh,pin_error[2,:,:],cmap='coolwarm')
+    plt.xticks(Xmesh)
+    plt.yticks(Ymesh)
+    plt.axis([0,15.0,0,15.0])
+    titlestr = "Power Shape Error, %s" % methods[2]
+    plt.title(titlestr)
+    plt.xlabel('Axial Height [cm]')
+    plt.ylabel('Power Shape Error (%)')
+    plt.clim([clim_lower_bound[2],clim_upper_bound[2]])
+    plt.colorbar()
+    #plt.show()
+
+    filename = "takeda_30cm_power_error_%s.png" % methods[2]
+    #filename = "takeda_30cm_power_error_%s.eps" % methods[2]
+    dirname = "figures/%s/" % methods[2]
+    call(["mkdir","-p",dirname])
+    savename = "%s/%s" % (dirname,filename)
+    #fig.savefig(savename, format='eps', dpi=1000)
+    fig.savefig(savename, format='png')
+    plt.close(fig)
+
+    fig = plt.figure(3)
+    plt.rc('font', size = 16)
+    plt.pcolor(Xmesh,Ymesh,pin_error[3,:,:],cmap='coolwarm')
+    plt.xticks(Xmesh)
+    plt.yticks(Ymesh)
+    plt.axis([0,15.0,0,15.0])
+    titlestr = "Power Shape Error, %s" % methods[3]
+    plt.title(titlestr)
+    plt.xlabel('Axial Height [cm]')
+    plt.ylabel('Power Shape Error (%)')
+    plt.clim([clim_lower_bound[3],clim_upper_bound[3]])
+    plt.colorbar()
+    #plt.show()
+
+    filename = "takeda_30cm_power_error_%s.png" % methods[3]
+    #filename = "takeda_30cm_power_error_%s.eps" % methods[3]
+    dirname = "figures/%s/" % methods[3]
+    call(["mkdir","-p",dirname])
+    savename = "%s/%s" % (dirname,filename)
+    #fig.savefig(savename, format='eps', dpi=1000)
+    fig.savefig(savename, format='png')
+    plt.close(fig)
+
+    fig = plt.figure(4)
+    plt.rc('font', size = 16)
+    plt.pcolor(Xmesh,Ymesh,pin_error[4,:,:],cmap='coolwarm')
+    plt.xticks(Xmesh)
+    plt.yticks(Ymesh)
+    plt.axis([0,15.0,0,15.0])
+    titlestr = "Power Shape Error, SP3, 1.7 mm"
+    plt.title(titlestr)
+    plt.xlabel('Axial Height [cm]')
+    plt.ylabel('Power Shape Error (%)')
+    plt.clim([clim_lower_bound[4],clim_upper_bound[4]])
+    plt.colorbar()
+    #plt.show()
+
+    filename = "takeda_30cm_power_error_SP3_1.7mm.png"
+    #filename = "takeda_30cm_power_error_SP3_1.7mm.eps"
+    dirname = "figures/SP3/"
+    call(["mkdir","-p",dirname])
+    savename = "%s/%s" % (dirname,filename)
+    #fig.savefig(savename, format='eps', dpi=1000)
+    fig.savefig(savename, format='png')
+    plt.close(fig)
+
+    fig = plt.figure(5)
+    plt.rc('font', size = 16)
+    plt.pcolor(Xmesh,Ymesh,pin_error[5,:,:],cmap='coolwarm')
+    plt.xticks(Xmesh)
+    plt.yticks(Ymesh)
+    plt.axis([0,15.0,0,15.0])
+    titlestr = "Power Shape Error, SP1, 1.7 mm"
+    plt.title(titlestr)
+    plt.xlabel('Axial Height [cm]')
+    plt.ylabel('Power Shape Error (%)')
+    plt.clim([clim_lower_bound[5],clim_upper_bound[5]])
+    plt.colorbar()
+    #plt.show()
+
+    filename = "takeda_30cm_power_error_SP1_1.7mm.png"
+    #filename = "takeda_30cm_power_error_SP1_1.7mm.eps"
+    dirname = "figures/SP1/"
+    call(["mkdir","-p",dirname])
+    savename = "%s/%s" % (dirname,filename)
+    #fig.savefig(savename, format='eps', dpi=1000)
+    fig.savefig(savename, format='png')
+    plt.close(fig)
+
+
+    ###########################################
+    # comparing axial and radial power shapes #
+    ###########################################
+
+    axial_ref = np.zeros([nz])
+    radial_ref = np.zeros([nx])
+    axial_power = np.zeros([nmeth+2,nz])
+    radial_power = np.zeros([nmeth+2,nz])
+    axial_error = np.zeros([nmeth+2,nz])
+    radial_error = np.zeros([nmeth+2,nz])
+
+    ### radial power
+    for ix in range(0,nx):
+        tmpsum = 0.0
+        for iz in range(0,nz):
+            tmpsum = tmpsum + ref_pinpow[ix,iz] 
+        radial_ref[ix] = tmpsum
+    radnorm = float(nx)/sum(radial_ref[:])
+    radial_ref[:] = radial_ref[:]*radnorm
+
+    ### axial power
+    for iz in range(0,nz):
+        tmpsum = 0.0
+        for ix in range(0,nx):
+            tmpsum = tmpsum + ref_pinpow[ix,iz] 
+        axial_ref[iz] = tmpsum
+    axnorm = float(nz)/sum(axial_ref[:])
+    axial_ref[:] = axial_ref[:]*axnorm
+
+    for im in range(0,nmeth): 
+        #print "method = %s " % methods[im]
+
+        ### radial power
+        for ix in range(0,nx):
+            tmpsum = 0.0
+            for iz in range(0,nz):
+                tmpsum = tmpsum + mpact_pinpow[im,ix,iz] 
+            radial_power[im,ix] = tmpsum*radnorm
+            radial_error[im,ix] = radial_power[im,ix] - radial_ref[ix]
+
+        ### axial power
+        for iz in range(0,nz):
+            tmpsum = 0.0
+            for ix in range(0,nx):
+                tmpsum = tmpsum + mpact_pinpow[im,ix,iz] 
+            axial_power[im,iz] = tmpsum*axnorm
+            axial_error[im,iz] = axial_power[im,iz] - axial_ref[iz]
+
+    ### SP3 ###
+    ### radial power
+    for ix in range(0,nx):
+        tmpsum = 0.0
+        for iz in range(0,nz):
+            tmpsum = tmpsum + sp3_pinpow[ix,iz] 
+        radial_power[nmeth,ix] = tmpsum*radnorm
+        radial_error[nmeth,ix] = radial_power[nmeth,ix] - radial_ref[ix]
+
+    ### axial power
+    for iz in range(0,nz):
+        tmpsum = 0.0
+        for ix in range(0,nx):
+            tmpsum = tmpsum + sp3_pinpow[ix,iz] 
+        axial_power[nmeth,iz] = tmpsum*axnorm
+        axial_error[nmeth,iz] = axial_power[nmeth,iz] - axial_ref[iz]
+
+    ### SP1 ###
+    ### radial power
+    for ix in range(0,nx):
+        tmpsum = 0.0
+        for iz in range(0,nz):
+            tmpsum = tmpsum + diffusion_pinpow[ix,iz] 
+        radial_power[nmeth+1,ix] = tmpsum*radnorm
+        radial_error[nmeth+1,ix] = radial_power[nmeth+1,ix] - radial_ref[ix]
+
+    ### axial power
+    for iz in range(0,nz):
+        tmpsum = 0.0
+        for ix in range(0,nx):
+            tmpsum = tmpsum + diffusion_pinpow[ix,iz] 
+        axial_power[nmeth+1,iz] = tmpsum*axnorm
+        axial_error[nmeth+1,iz] = axial_power[nmeth+1,iz] - axial_ref[iz]
+
+    ### plot the power shapes ###
+    nxhist = 30
+    nzhist = 30
+    plot_x = np.zeros([nxhist])
+    plot_y = np.zeros([nzhist])
+    plot_radial_power = np.zeros([nmeth+3,nxhist])
+    plot_radial_diff  = np.zeros([nmeth+2,nxhist])
+    plot_axial_power = np.zeros([nmeth+3,nzhist])
+    plot_axial_diff  = np.zeros([nmeth+2,nzhist])
+    plot_x = make_hist(Xmesh,nx)
+    plot_y = make_hist(Ymesh,nz)
+
+    for im in range(0,nmeth+2):
+        #diff = radial_error[im,:]
+        diff = 100.0*radial_error[im,:]/radial_ref[:]
+        #plot_diff[im,:] = 100.0*make_hist(diff,nx)
+        plot_radial_diff[im,:] = make_hist(diff,nx)
+        plot_radial_power[im+1,:] = make_hist(radial_power[im,:],nx)
+
+        #diff = axial_error[im,:]
+        diff = 100.0*axial_error[im,:]/axial_ref[:]
+        #plot_diff[im,:] = 100.0*make_hist(diff,nz)
+        plot_axial_diff[im,:] = make_hist(diff,nz)
+        plot_axial_power[im+1,:] = make_hist(axial_power[im,:],nx)
+
+    ### Errors ###
+
+    fig = plt.figure(6)
+    plt.rc('font', size = 16)
+
+    isotlline = plt.plot(plot_x[:],plot_radial_diff[0,:], 'g-', linewidth='2.0', label='P3 ISO TL')
+    isoxsline = plt.plot(plot_x[:],plot_radial_diff[1,:], 'b-', linewidth='2.0', label='P3 ANISO TL')
+    sp3line = plt.plot(plot_x[:],plot_radial_diff[nmeth,:], 'r-', linewidth='2.0', label='SP3')
+    #sp1line = plt.plot(plot_x[:],plot_radial_diff[nmeth+1,:], '-', color="slateblue", linewidth='2.0', label='SP3')
+
+    titlestr = "Axially Integrated Radial Power"
+    plt.title(titlestr)
+    plt.xlabel('Radial Height [cm]')
+    plt.ylabel('Radial Power Error (%)')
+    #plt.legend(['P3 ISO TL','P3 ANISO TL','SP3','DIFF'],loc='best')
+    plt.legend(['P3 ISO TL','P3 ANISO TL','SP3'],loc='best')
+    plt.axis([0,nx,-1.0,1.0])
+    #plt.show()
+
+    filename = "takeda_30cm_radial_power_error.png"
+    #filename = "takeda_30cm_radial_power_error.eps"
+    dirname = "figures/radial/"
+    call(["mkdir","-p",dirname])
+    savename = "%s/%s" % (dirname,filename)
+    #fig.savefig(savename, format='eps', dpi=1000)
+    fig.savefig(savename, format='png')
+    plt.close(fig)
+
+    fig = plt.figure(7)
+    plt.rc('font', size = 16)
+
+    isotlline = plt.plot(plot_y[:],plot_axial_diff[0,:], 'g-', linewidth='2.0', label='P3 ISO TL')
+    isoxsline = plt.plot(plot_y[:],plot_axial_diff[1,:], 'b-', linewidth='2.0', label='P3 ANISO TL')
+    sp3line = plt.plot(plot_y[:],plot_axial_diff[nmeth,:], 'r-', linewidth='2.0', label='SP3')
+    #sp1line = plt.plot(plot_y[:],plot_axial_diff[nmeth+1,:], '-', color="slateblue", linewidth='2.0', label='SP3')
+
+    titlestr = "Radially Integrated Axial Power"
+    plt.title(titlestr)
+    plt.xlabel('Axial Height [cm]')
+    plt.ylabel('Axial Power Error (%)')
+    #plt.legend(['P3 ISO TL','P3 ANISO TL','SP3','DIFF'],loc='best')
+    plt.legend(['P3 ISO TL','P3 ANISO TL','SP3'],loc='best')
+    plt.axis([0,nz,-1.0,1.5])
+    #plt.show()
+
+    filename = "takeda_30cm_axial_power_error.png"
+    #filename = "takeda_30cm_axial_power_error.eps"
+    dirname = "figures/axial"
+    call(["mkdir","-p",dirname])
+    savename = "%s/%s" % (dirname,filename)
+    #fig.savefig(savename, format='eps', dpi=1000)
+    fig.savefig(savename, format='png')
+    plt.close(fig)
+
+    ### Powers ###
+
+    fig = plt.figure(8)
+    plt.rc('font', size = 16)
+
+    isotlline = plt.plot(plot_x[:],plot_radial_power[0,:], 'g-', linewidth='2.0', label='P3 ISO TL')
+    isoxsline = plt.plot(plot_x[:],plot_radial_power[1,:], 'b-', linewidth='2.0', label='P3 ANISO TL')
+    sp3line = plt.plot(plot_x[:],plot_radial_power[nmeth,:], 'r-', linewidth='2.0', label='SP3')
+    sp1line = plt.plot(plot_x[:],plot_radial_power[nmeth+1,:], '-', color="slateblue", linewidth='2.0', label='SP3')
+
+    titlestr = "Axially Integrated Radial Power"
+    plt.title(titlestr)
+    plt.xlabel('Radial Height [cm]')
+    plt.ylabel('Radial Power Error (%)')
+    plt.legend(['P3 ISO TL','P3 ANISO TL','SP3','DIFF'],loc='best')
+    #plt.legend(['P3 ISO TL','P3 ANISO TL','SP3'],loc='best')
+    plt.axis([0,nx,0.8,1.3])
+    #plt.show()
+
+    filename = "takeda_30cm_radial_power.png"
+    #filename = "takeda_30cm_radial_power.eps"
+    dirname = "figures/radial/"
+    call(["mkdir","-p",dirname])
+    savename = "%s/%s" % (dirname,filename)
+    #fig.savefig(savename, format='eps', dpi=1000)
+    fig.savefig(savename, format='png')
+    plt.close(fig)
+
+    fig = plt.figure(9)
+    plt.rc('font', size = 16)
+
+    isotlline = plt.plot(plot_y[:],plot_axial_power[0,:], 'g-', linewidth='2.0', label='P3 ISO TL')
+    isoxsline = plt.plot(plot_y[:],plot_axial_power[1,:], 'b-', linewidth='2.0', label='P3 ANISO TL')
+    sp3line = plt.plot(plot_y[:],plot_axial_power[nmeth,:], 'r-', linewidth='2.0', label='SP3')
+    sp1line = plt.plot(plot_y[:],plot_axial_power[nmeth+1,:], '-', color="slateblue", linewidth='2.0', label='SP3')
+
+    titlestr = "Radially Integrated Axial Power"
+    plt.title(titlestr)
+    plt.xlabel('Axial Height [cm]')
+    plt.ylabel('Axial Power Error (%)')
+    plt.legend(['P3 ISO TL','P3 ANISO TL','SP3','DIFF'],loc='best')
+    #plt.legend(['P3 ISO TL','P3 ANISO TL','SP3'],loc='best')
+    plt.axis([0,nz,0.8,1.3])
+    #plt.show()
+
+    filename = "takeda_30cm_axial_power.png"
+    #filename = "takeda_30cm_axial_power.eps"
+    dirname = "figures/axial"
+    call(["mkdir","-p",dirname])
+    savename = "%s/%s" % (dirname,filename)
+    #fig.savefig(savename, format='eps', dpi=1000)
+    fig.savefig(savename, format='png')
+    plt.close(fig)
 
     ref.close()
